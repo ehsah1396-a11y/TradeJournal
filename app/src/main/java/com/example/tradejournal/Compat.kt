7631d3e0b73feff513e10f545ee7b3174e3daa66
ehsah1396-a11y <ehsah1396@gmail.com>
2026-09-24 12:16:58 +0330

    Delete app/src/main/java/com/example/tradejournal directory

--- app/src/main/java/com/example/tradejournal/Compat.kt
+++
@@ -1,7 +0,0 @@
-package com.example.tradejournal
-
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-
-@Composable
-fun <T> rememberSaveable(init: () -> T): T = remember { init() }
