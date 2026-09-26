b8e4cfbfe49581b11dc254a06ddb7e4e89811140
ehsah1396-a11y <ehsah1396@gmail.com>
2026-09-26 20:32:24 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,10 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions { jvmTarget = "17" }
-    buildFeatures { compose = true }
+    buildFeatures {
+        compose = true
+        aidl = true   // برای IInAppBillingService.aidl (بخش خرید پرو از بازار/مایکت) لازم است
+    }
 }
 
 dependencies {
